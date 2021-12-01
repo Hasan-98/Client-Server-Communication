33c3f869c0e0b01e6e4d9ded6097d8f951e812bf
Hasan-98 <hasanmehmood458@gmail.com>
2021-12-01 15:25:23 +0500

    Remove ignored files

---
+++ src/ServerThread.java
@@ -0,0 +1,32 @@
+import java.io.*;
+import java.net.*;
+import java.util.concurrent.CountDownLatch;
+
+public class ServerThread extends Thread {
+	private Socket socket;
+	private CountDownLatch latch;
+
+	public ServerThread(Socket socket, CountDownLatch countDownLatch) {
+
+		this.socket = socket;
+		latch = countDownLatch;
+	}
+
+	public void run() {
+		try {
+			DataInputStream dis = new DataInputStream(socket.getInputStream());
+			String str = (String) dis.readUTF();
+			System.out.println("Client: " + str);
+			int delay = 5000;
+			Thread.sleep(delay);
+			DataOutputStream data = new DataOutputStream(socket.getOutputStream());
+			data.writeUTF("I AM ALIVE");
+
+			latch.countDown();
+
+		} catch (Exception e) {
+			e.getMessage();
+		}
+	}
+
+}

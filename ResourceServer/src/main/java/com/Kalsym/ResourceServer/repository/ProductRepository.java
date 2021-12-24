d267b59155bcfd0a7fb4d7213a8e39fa6ef80ead
Hasan-98 <hasanmehmood458@gmail.com>
2021-12-24 16:39:41 +0500

    Product Service

--- ResourceServer/src/main/java/com/Kalsym/ResourceServer/repository/ProductRepository.java
+++
@@ -1,9 +0,0 @@
-package com.Kalsym.ResourceServer.repository;
-
-import com.Kalsym.ResourceServer.model.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ProductRepository extends JpaRepository<Product, Integer> {
-}
